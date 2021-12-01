9f184e1c5024bcbed309f466d0af6badc2453666
Chandra Holt <chandra_holt@hotmail.com>
2021-11-30 18:41:59 -0800

    Added save button to each line. Added work times in HTML. TODO add form to each row for user to fill in

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,18 @@
 console.log("Oh hey there");
-
+//Global variables
 var today = document.getElementById("currentDay");
+var row = document.getElementsByClassName("row");
+var saveButton = document.getElementById("saveBtn")
+var userInput = localStorage.getItem("");
 
+//Displays the current date on the page
 today.textContent += moment().format('MMMM Do, YYYY');
 
-document.getElementById("saveBtn").addEventListener("click", saveEvent)
+//Adds function to 'save' button
+saveButton.addEventListener("click", saveEvent);
 
-function saveEvent {
-    
+//
+function saveEvent() {
+    console.log("Saved!");
+    localStorage.setItem("");
 }
